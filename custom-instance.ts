0c166590a3f3dc847f15288d27738ab54ad08ab9
Cheryl Carpenter <ccarpenter@tirerack.com>
2023-11-15 15:54:33 -0600

    full custom instance

--- custom-instance.ts
+++ custom-instance.ts
@@ -1,21 +1,49 @@
-import axios from "axios";
 import Axios, { AxiosError, AxiosRequestConfig } from "axios";
 
-const baseURL = "https://test"; // use your own URL here or environment variable
+const baseURL = process.env.NEXT_PUBLIC_DOMAIN_URL; // use your own URL here or environment variable
 export const AXIOS_INSTANCE = Axios.create({ baseURL: baseURL });
 
-export const customInstance = <T>(config: AxiosRequestConfig): Promise<T> => {
-  const source = Axios.CancelToken.source();
-  const promise = AXIOS_INSTANCE({ ...config, cancelToken: source.token }).then(
-    ({ data }) => data
-  );
-
-  // @ts-ignore
-  promise.cancel = () => {
-    source.cancel("Query was cancelled by React Query");
+export const customInstance = async <T>({
+  url,
+  method,
+  params,
+  data,
+}: {
+  url: string;
+  method: "get" | "post" | "put" | "delete" | "patch";
+  params?: any;
+  data?: BodyType<any>;
+  responseType?: string;
+  headers?: {
+    "Access-Control-Allow-Origin": "*";
+    "Content-Type": "application/json";
+    "Access-Control-Allow-Headers": "Origin, Content-Type, Accept, Methods, authorization, x-header-hello";
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS";
+    "x-header-hello": "world";
   };
+}): Promise<T> => {
+  let response;
 
-  return promise;
+  if (params) {
+    response = await fetch(`${baseURL}${url}?` + new URLSearchParams(params), {
+      method,
+      ...(data ? { body: JSON.stringify(data) } : {}),
+    });
+    return response.json();
+  } else {
+    response = await fetch(`${baseURL}${url}`, {
+      method,
+      ...(data ? { body: JSON.stringify(data) } : {}),
+    });
+    return response.json();
+  }
 };
 
 export default customInstance;
+
+// In some case with react-query and swr you want to be able to override the return error type so you can also do it here like this
+export type ErrorType<Error> = AxiosError<Error>;
+// In case you want to wrap the body type (optional)
+// (if the custom instance is processing data before sending it, like changing the case for example)
+// export type BodyType<BodyData> = CamelCase<BodyType>;
+export type BodyType<BodyData> = BodyData & { headers?: any };
